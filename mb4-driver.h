69464f0eb2d0808b88d21d0208ab3c3df905f2d6
Michael George <michaelgeorge180@gmail.com>
2016-11-28 17:21:25 -0800

    added license and comment header to source code files.

--- mb4-driver.h
+++ mb4-driver.h
@@ -1,7 +1,44 @@
-// mb4-driver.h
-// Class for interfacing with an IC-MB4 chip
-// Michael George
-// 10/21/16
+/* mb4-driver.h
+   Class for interfacing with an IC-MB4 master IC. 
+   
+   California Polytechnic State University, San Luis Obispo
+   In partial fulfillment of the requirements for a bachelor's 
+   degree from the department of Mechanical Engineering. 
+
+   Michael George
+   10/21/16
+
+   This code comes without any warrenty or guarantee from the author. 
+   Any usage is at the discression of the user, and should be done 
+   at their own risk. 
+
+   This software is hereby licensed under the Modified BSD License.
+
+   Copyright (c) 2016, Michael George
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+       * Redistributions of source code must retain the above copyright
+         notice, this list of conditions and the following disclaimer.
+       * Redistributions in binary form must reproduce the above copyright
+         notice, this list of conditions and the following disclaimer in the
+         documentation and/or other materials provided with the distribution.
+       * Neither the name of the Accumulator Volume Sensing Team nor the
+         names of its contributors may be used to endorse or promote products
+         derived from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+   DISCLAIMED. IN NO EVENT SHALL ACCUMULATOR VOLUME SENSING TEAM BE LIABLE FOR ANY
+   DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #include <SPI.h>
 
